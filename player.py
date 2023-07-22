677397deb9cbd5f85cc1e1a766f173eabebbf1dd
wiloser <1229054438@qq.com>
2023-07-22 17:58:05 +0800

    导入资源

--- player.py
+++ player.py
@@ -1,21 +1,36 @@
+import random
 import pygame
 from settings import *
+from support import *
+
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, group):
         super().__init__(group)
 
+        self.import_assets()
+        self.status = 'down_axe'
+
         self.image = pygame.Surface((64, 32))
         self.image.fill('green')
-        self.rect = self.image.get_rect(center = pos)
+        self.rect = self.image.get_rect(center=pos)
 
         self.direction = pygame.math.Vector2()
         self.pos = pygame.math.Vector2(self.rect.center)
         self.speed = 200
 
+    def import_assets(self):
+        self.animations = {'up': [], 'down': [], 'left': [], 'right': []}
+
+        for animation in self.animations.keys():
+            full_path = images_path + animation
+            self.animations[animation] = import_folder(full_path)
     def input(self):
         keys = pygame.key.get_pressed()
-
+        red = random.randint(0, 255)
+        green = random.randint(0, 255)
+        blue = random.randint(0, 255)
+        self.image.fill((red, green, blue))
         if keys[pygame.K_UP]:
             self.direction.y = -1
             print('up')
@@ -33,11 +48,15 @@ class Player(pygame.sprite.Sprite):
             print('left')
         else:
             self.direction.x = 0
+        pygame.event.pump()
 
     def move(self, dt):
+        if self.direction.magnitude() > 0:
+            self.direction = self.direction.normalize()
+
         self.pos += self.direction * self.speed * dt
         self.rect.center = self.pos
-
+        print(self.direction)
     def update(self, dt):
         self.input()
         self.move(dt)
--- settings.py
+++ settings.py
@@ -1,2 +1,4 @@
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
+
+images_path = 'assets/graphics/character/'
